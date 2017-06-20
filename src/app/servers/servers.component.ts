6536e234dd8478cdd6e446e9c3e5d0e3f67de13c
Rohit Raj Thirumurthy <rohit.raj.thirumurthy@sap.com>
2017-06-20 14:19:59 -0700

    DataBinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // templateUrl: './servers.component.html'
-  template: '<app-server></app-server><app-server></app-server>'
+  templateUrl: './servers.component.html'
+  // template: '<app-server></app-server><app-server></app-server>'
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No Server was created"
+  serverName = 'TestServer';
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server Was Created with Name:" + this.serverName;      
+  }
+
+  onUpdateServer(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;      
   }
 
 }
